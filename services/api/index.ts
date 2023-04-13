511b889e7439014ade929df6356543bdb1a7b59e
Cristian URSU <cristian.ursu.2001@gmail.com>
2023-04-13 14:50:02 +0200

    database connection

--- services/api/index.ts
+++ services/api/index.ts
@@ -1,4 +1,19 @@
 import * as express from 'express'
+import * as mysql from "mysql2";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const con = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME
+});
+
+con.connect((err) => {
+    if (err) throw err;
+    console.log("Connected to database !");
+})
 
 const app = express()
 
@@ -6,6 +21,6 @@ app.get("/", (req, res) => {
     res.status(200).send('Server up')
 })
 
-app.listen(process.env.PORT, () => {
+app.listen("3000", () => {
     console.log("Server up");
 })
