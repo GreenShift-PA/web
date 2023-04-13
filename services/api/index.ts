12a7e8dfdf73adc4eb83610622ea9b0b00bedaa5
Cristian URSU <cristian.ursu.2001@gmail.com>
2023-04-13 16:40:37 +0200

    database get user + connection

--- services/api/index.ts
+++ services/api/index.ts
@@ -1,26 +1,20 @@
+import * as dotenv from "dotenv"
+dotenv.config()
+
 import * as express from 'express'
-import * as mysql from "mysql2";
-import * as dotenv from "dotenv";
-dotenv.config();
-
-const con = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    database: process.env.DB_NAME
-});
-
-con.connect((err) => {
-    if (err) throw err;
-    console.log("Connected to database !");
-})
+import {getUsers} from './database/user.database'
 
 const app = express()
 
 app.get("/", (req, res) => {
-    res.status(200).send('Server up')
+    res.status(200).send("Server Up")
 })
 
-app.listen("3000", () => {
+app.get('/users', async (req, res) => {
+    const users = await getUsers()
+    res.send(users)
+})
+
+app.listen(process.env.PORT, () => {
     console.log("Server up");
 })
