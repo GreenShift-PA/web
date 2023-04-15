9ce3daa15ba123f70dddfbed0ef05293194f15bf
Cristian URSU <cristian.ursu.2001@gmail.com>
2023-04-15 12:40:27 +0200

    new user controller + CRUD routes for user

--- services/api/index.ts
+++ services/api/index.ts
@@ -2,7 +2,15 @@ import * as dotenv from "dotenv"
 dotenv.config()
 
 import * as express from 'express'
-import {getUsers} from './database/user.database'
+import { UserController } from "./controller/user.controller"
+const mysql = require('mysql2');
+
+export const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME
+}).promise()
 
 const app = express()
 
@@ -10,10 +18,11 @@ app.get("/", (req, res) => {
     res.status(200).send("Server Up")
 })
 
-app.get('/users', async (req, res) => {
-    const users = await getUsers()
-    res.send(users)
-})
+const userController = new UserController(pool)
+
+// Route : /user/...
+app.use(userController.path, userController.buildRouter())
+
 
 app.listen(process.env.PORT, () => {
     console.log("Server up");
