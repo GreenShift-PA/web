6a656807f9811608a3e8f4698e9dc60eb6d29861
paschyz <paschy2002@gmail.com>
2023-05-03 20:02:46 +0200

    added icons and transitions

---
+++ services/frontend/src/app/components/vertical-navbar/vertical-navbar.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VerticalNavbarComponent } from './vertical-navbar.component';
+
+describe('VerticalNavbarComponent', () => {
+  let component: VerticalNavbarComponent;
+  let fixture: ComponentFixture<VerticalNavbarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ VerticalNavbarComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(VerticalNavbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ services/frontend/src/app/components/vertical-navbar/vertical-navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-vertical-navbar',
+  templateUrl: './vertical-navbar.component.html',
+  styleUrls: ['./vertical-navbar.component.css']
+})
+
+export class VerticalNavbarComponent {
+
+}
