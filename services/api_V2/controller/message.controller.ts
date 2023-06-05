a919c965978d4d4329c57747c9548c0cb33c3b3c
Cristian URSU <cristian.ursu.2001@gmail.com>
2023-06-05 16:05:35 +0200

    get conversation

--- services/api_V2/controller/message.controller.ts
+++ services/api_V2/controller/message.controller.ts
@@ -4,6 +4,7 @@ import { Message, MessageModel, UserModel } from "../models"
 import * as express from "express"
 import { Router, Response, Request } from "express"
 import { checkBody, checkUserToken } from "../middleware"
+import { checkQuery } from "../middleware/query.middleware"
 
 export class MessageController {
 
@@ -51,12 +52,47 @@ export class MessageController {
 
         res.status(201).json(message)
         return 
-
-
     }
+
+    readonly queryGetConv = {
+        "contact" : "string"
+    }
+
+    getConversation = async (req: Request, res: Response): Promise<void> => {
+        let contact
+
+        try{
+            contact = await UserModel.findById(req.query.contact)
+            if(!contact){
+                res.status(404).json({"message" : "User not found"})
+            }
+        }catch(err){
+            res.status(400).json({"message": "This is not an user Id"})
+            return 
+        }
+
+        try{
+            const conversation = await MessageModel.find({$or:[
+                {from: req.user, to:contact},
+                {from: contact, to:req.user}
+            ]})
+            if(!contact){
+                res.status(204).json({"message": "No conversation with this contact"})
+                return 
+            }
+            res.status(200).json(conversation)
+            return
+        }catch(err){
+            console.log(err)
+            res.status(500).end()
+            return 
+        }
+    }
+
 
     buildRouter = (): Router => {
         const router = express.Router()
+        router.get('/', checkUserToken(), checkQuery(this.queryGetConv), this.getConversation.bind(this))
         router.post('/', express.json(), checkUserToken(), checkBody(this.paramsSendMessage), this.sendMessage.bind(this))
         return router
     }
