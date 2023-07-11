4f82e610f11e6a607354e8c2651af09dc602b0fa
paschyz <paschy2002@gmail.com>
2023-07-11 21:54:22 +0200

    added second list completed

--- services/frontend/src/app/components/todos/todos.component.ts
+++ services/frontend/src/app/components/todos/todos.component.ts
@@ -6,31 +6,69 @@ import { Todo } from 'src/app/models/Todo';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
-  todos:Todo[] | undefined;
+  todos:Todo[]=[];
+  todos_done:Todo[]=[];
   inputTodo:string="";
   constructor(){
 
   }
 
   ngOnInit(): void {
-      this.todos=[
-        {
-          content:'First todo',
-          completed:false
-        },        {
-          content:'Second todo',
-          completed:false
-        },
-      ]
+    this.todos = [
+      {
+        content: 'First short task',
+        completed: false
+      },
+      {
+        content: 'Second medium-length task',
+        completed: false
+      },
+      {
+        content: 'Third long task with more content and details',
+        completed: false
+      },
+      {
+        content: 'Fourth task',
+        completed: false
+      },
+    ];
+  
+    this.todos_done = [
+      {
+        content: 'First completed task',
+        completed: true
+      },
+      {
+        content: 'Second completed task',
+        completed: true
+      },
+      {
+        content: 'Third completed task',
+        completed: true
+      },
+    ];
   }
 
-  toggleDone(id:number){
-      this.todos?.map((v,i)=>{
-        if(i==id) v.completed=!v.completed;
-        return v;
-      })
+  toggleDone_todos(id: number) {
+    const todo = this.todos[id];
+    todo.completed = !todo.completed;
+  
+    if (todo.completed) {
+      this.todos_done.push(todo);
+      this.todos = this.todos.filter((_, i) => i !== id);
+    }
   }
-
+  
+  toggleDone_todos_done(id: number) {
+    const todo = this.todos_done[id];
+    todo.completed = !todo.completed;
+  
+    if (!todo.completed) {
+      this.todos.push(todo);
+      this.todos_done = this.todos_done.filter((_, i) => i !== id);
+    }
+  }
+  
   deleteTodo(id:number){
     this.todos= this.todos?.filter((v,i)=> i!==id);
 
