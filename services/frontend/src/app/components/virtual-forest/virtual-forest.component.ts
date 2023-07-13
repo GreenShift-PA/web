948030cca0cb65a32a90a2eb74adf035a3f37f5b
Cristian URSU <cristian.ursu.2001@gmail.com>
2023-07-13 10:37:24 +0200

    map relief

--- services/frontend/src/app/components/virtual-forest/virtual-forest.component.ts
+++ services/frontend/src/app/components/virtual-forest/virtual-forest.component.ts
@@ -1,9 +1,9 @@
 import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import * as THREE from "three"
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { mergeGeometries } from 'three/examples/jsm/utils/BufferGeometryUtils';
 import * as dat from 'lil-gui'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-
+import { createNoise2D } from 'simplex-noise'
 @Component({
 selector: 'app-virtual-forest',
 templateUrl: './virtual-forest.component.html',
@@ -17,6 +17,55 @@ export class VirtualForestComponent implements OnInit{
 
 	tileToPosition(tileX:number, tileY:number){
 		return new THREE.Vector2((tileX + (tileY % 2) * 0.5) * 1.77, tileY * 1.535)
+	}
+
+	MAX_HEIGHT:number = 10
+	STONE_HEIGHT: number = this.MAX_HEIGHT * 0.8
+	DIRT_HEIGHT: number = this.MAX_HEIGHT * 0.7
+	GRASS_HEIGHT: number = this.MAX_HEIGHT * 0.5
+	SAND_HEIGHT: number = this.MAX_HEIGHT * 0.3
+	DIRT2_HEIGHT: number = this.MAX_HEIGHT * 0
+
+	// INFO: If the is no type any its can't compile
+	stoneGeo:any = new THREE.BoxGeometry(0,0,0)
+	dirstGeo:any = new THREE.BoxGeometry(0,0,0)
+	dirst2Geo:any = new THREE.BoxGeometry(0,0,0)
+	sandGeo:any = new THREE.BoxGeometry(0,0,0)
+	grassGeo:any = new THREE.BoxGeometry(0,0,0)
+
+	hexGeometry = (height: number, position: THREE.Vector2) => {
+		let geo = new THREE.CylinderGeometry(1, 1, height, 6, 1 , false)
+		geo.translate(position.x, height * 0.5, position.y)
+
+		return geo
+	}
+
+	makeHex = (height:number, position:THREE.Vector2) => {
+		const geo = this.hexGeometry(height, position)
+
+		if (height > this.STONE_HEIGHT){
+			this.stoneGeo = mergeGeometries([this.stoneGeo, geo])
+		}else if (height > this.DIRT_HEIGHT) {
+			this.dirstGeo = mergeGeometries([this.dirstGeo, geo])
+		}else if (height > this.GRASS_HEIGHT) {
+			this.grassGeo = mergeGeometries([this.grassGeo, geo])
+		}else if (height > this.SAND_HEIGHT) {
+			this.sandGeo = mergeGeometries([this.sandGeo, geo])
+		}else if (height > this.DIRT2_HEIGHT) {
+			this.dirst2Geo = mergeGeometries([this.dirst2Geo, geo])
+		}
+	}
+
+	// TODO: Change any into a type 	
+	hexMesh = (geo:any, map:any) => {
+		const mat = new THREE.MeshPhysicalMaterial({
+			map
+		})
+		const mesh = new THREE.Mesh(geo, mat)
+		mesh.castShadow = true
+		mesh.receiveShadow = true
+
+		return mesh
 	}
 	
 	createThreeJsBox = () => {
@@ -33,6 +82,17 @@ export class VirtualForestComponent implements OnInit{
 		const parameters_scene = {
 			bg_color: "#ffeecc"
 		}
+
+		// Textures Loader
+		const textureLoader = new THREE.TextureLoader()
+		const textures = {
+			dirt: textureLoader.load("/assets/map/dirt.png"),
+			dirt2: textureLoader.load("/assets/map/dirt2.jpg"),
+			grass: textureLoader.load("/assets/map/grass.jpg"),
+			sand: textureLoader.load("/assets/map/sand.jpg"),
+			water: textureLoader.load("/assets/map/water.jpg"),
+			stone: textureLoader.load("/assets/map/stone.png"),
+		}
 		
 		// Set up Scene
 		const scene = new THREE.Scene()
@@ -42,35 +102,56 @@ export class VirtualForestComponent implements OnInit{
 			scene.background = new THREE.Color(parameters_scene.bg_color)
 		})
 
-		const hexagon_group = new THREE.Group()
-		const height : number = 3
-
-		const hexagon_grometry = new THREE.CylinderGeometry(1, 1, height, 6, 1, false)
-		const hexagon_material = new THREE.MeshStandardMaterial({color: '#B2B6B1'})
+		const noise2D = createNoise2D();
 
 		for (let i = -10; i <= 10; i++){
 			for (let j = -10; j <= 10; j++){
-				const hexa = new THREE.Mesh(hexagon_grometry, hexagon_material)
-				const hexa_position: THREE.Vector2 = this.tileToPosition(i, j)
-				
-				if (hexa_position.length() > 16) continue
-				
-				hexa.position.set(hexa_position.x, height * 0.5, hexa_position.y)
-				hexagon_group.add(hexa)
+				let position = this.tileToPosition(i, j)
+
+				if (position.length() > 16) continue
+
+				// The noise to make the relief
+				let noise = (noise2D(i * 0.1, j * 0.1) + 1) * 0.5
+				noise = Math.pow(noise, 2)
+
+				this.makeHex(noise * this.MAX_HEIGHT, position)
 			}
 		}
 
-		scene.add(hexagon_group)
+		const stoneMesh = this.hexMesh(this.stoneGeo, textures.stone)
+		const grassMesh = this.hexMesh(this.grassGeo, textures.grass)
+		const dirst2Mesh = this.hexMesh(this.dirst2Geo, textures.dirt2)
+		const dirstMesh = this.hexMesh(this.dirstGeo, textures.dirt)
+		const sandMesh = this.hexMesh(this.sandGeo, textures.sand)
+		scene.add(stoneMesh, grassMesh, dirst2Mesh, dirstMesh, sandMesh)
+
+
+
+		// Add water
+
 
 
 		// Ambient light
-		const ambientLight = new THREE.AmbientLight('#b9d5ff', 0.12)
+		const ambientLight = new THREE.AmbientLight('#b9d5ff', 0.15)
 		gui_anbiant_light.add(ambientLight, 'intensity').min(0).max(1).step(0.001).name("Intensity ambiant light")
 		scene.add(ambientLight)
 		
 		// Directional light
-		const directionalLight = new THREE.DirectionalLight('#b9d5ff', 0.12)
-		directionalLight.position.set(4, 10, 0)
+		const directionalLight = new THREE.DirectionalLight('#ffcb8e', 0.5)
+		directionalLight.position.set(10, 20, 10)
+
+		// Direction Light Shadow setup Create shadows
+		directionalLight.castShadow = true
+		// Attention : augmenter la resolution de la camera de la lumière consome beaucoup de performance
+		directionalLight.shadow.mapSize.width = 512
+		directionalLight.shadow.mapSize.height = 512 
+		directionalLight.shadow.camera.near = 0.5
+		directionalLight.shadow.camera.far = 500
+
+		directionalLight.shadow.camera.top = 15
+		directionalLight.shadow.camera.right = 15
+		directionalLight.shadow.camera.bottom = -15
+		directionalLight.shadow.camera.left = -15
 		
 		const directionalLightCameraHelper = new THREE.DirectionalLightHelper(directionalLight)
 		directionalLightCameraHelper.visible = false
@@ -78,9 +159,9 @@ export class VirtualForestComponent implements OnInit{
 
 		gui_directional_light.add(directionalLightCameraHelper, "visible")
 		gui_directional_light.add(directionalLight, 'intensity').min(0).max(1).step(0.001)
-		gui_directional_light.add(directionalLight.position, 'x').min(- 5).max(15).step(0.001)
-		gui_directional_light.add(directionalLight.position, 'y').min(- 5).max(15).step(0.001)
-		gui_directional_light.add(directionalLight.position, 'z').min(- 5).max(15).step(0.001)
+		gui_directional_light.add(directionalLight.position, 'x').min(-50).max(50).step(0.001)
+		gui_directional_light.add(directionalLight.position, 'y').min(-50).max(50).step(0.001)
+		gui_directional_light.add(directionalLight.position, 'z').min(-50).max(50).step(0.001)
 		scene.add(directionalLight)
 
 
@@ -123,6 +204,8 @@ export class VirtualForestComponent implements OnInit{
 		renderer.setSize(innerWidth, innerHeight)
 		renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 		renderer.setClearColor('#262837')
+		renderer.shadowMap.enabled = true
+		renderer.shadowMap.type = THREE.PCFSoftShadowMap
 
 
 		
