50495acd4284a8d0ba2af90262e8e5126f79db35
paschyz <paschy2002@gmail.com>
2023-07-17 04:57:01 +0200

    added verification if it's an image link, and image avatar

--- services/frontend/src/app/components/blog/blog.component.ts
+++ services/frontend/src/app/components/blog/blog.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component,OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { PostService } from 'src/app/services/post.service';
@@ -14,11 +15,34 @@ export class BlogComponent {
   commentText: string = '';
   
   commentLink: string = '';
+  
   posts:any=[];
 
 
 
-  constructor(private post: PostService,private user: UserService,private toastr:ToastrService) { }
+  constructor(private post: PostService,private user: UserService,private toastr:ToastrService,private http: HttpClient) {}
+
+  isImgUrl(url: string): Promise<boolean> {
+    return this.http.head(url, { observe: 'response' }).toPromise()
+      .then(res => {
+        const contentType = res?.headers.get('Content-Type');
+        return contentType !== null && contentType !== undefined && contentType.startsWith('image');
+      })
+      .catch(() => false);
+  }
+  validateImageURL() {
+    this.isImgUrl(this.commentLink).then(isValid => {
+      if (isValid) {
+        this.toastr.success("its an image !")
+        return
+      } else {
+ 
+        this.toastr.error("Not an image ! ");
+        return
+      }
+    });
+  }
+
   ngOnInit() {
     this.post.getAllPosts().subscribe(
       (response) => {
@@ -36,7 +60,7 @@ export class BlogComponent {
               console.log(userResponse);
               post.firstname = userResponse.firstname;
               post.lastname = userResponse.lastname;
-              post.profile= userResponse.profile;
+              post.image= userResponse.image;
             }
             
             ,
@@ -59,29 +83,30 @@ export class BlogComponent {
 
   submitComment() {
     // Check if the comment text is not empty
-    if (this.commentText.trim() !== '') {
-      // Send the request to the service
-      this.post.sendPost(this.commentText).subscribe(
-        (response) => {
-          // Handle the response if needed
-          console.log('Comment posted successfully');
-          // Reset the comment text after successful submission
+    this.validateImageURL()
+    // if (this.commentText.trim() !== '') {
+    //   // Send the request to the service
+    //   this.post.sendPost(this.commentText).subscribe(
+    //     (response) => {
+    //       // Handle the response if needed
+    //       console.log('Comment posted successfully');
+    //       // Reset the comment text after successful submission
 
-          this.commentText = '';
-          this.toastr.success("Message posted successfully.","",{
-            timeOut: 1000,
-          });
-          setTimeout(() => {
-            location.reload();
-          }, 1000);
-        },
-        (error) => {
-          // Handle the error if needed
-          console.error('Error posting comment:', error);
-          this.toastr.warning("Something went wrong");
+    //       this.commentText = '';
+    //       this.toastr.success("Message posted successfully.","",{
+    //         timeOut: 1000,
+    //       });
+    //       setTimeout(() => {
+    //         location.reload();
+    //       }, 1000);
+    //     },
+    //     (error) => {
+    //       // Handle the error if needed
+    //       console.error('Error posting comment:', error);
+    //       this.toastr.warning("Something went wrong");
 
-        }
-      );
-    }
+    //     }
+    //   );
+    // }
   }
 }
